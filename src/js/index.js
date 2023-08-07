9247b2c50448aecd94cfc2f157bba0e20af08191
vagner0795 <vagner-se@hotmail.com>
2023-08-07 15:45:25 -0300

    criada animação dos cards e adicionada a responsividade da pagina serviços

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+// ANIMAÇÕES DO SITE
+
+ScrollReveal().reveal(".box-card-servicos", {
+  duration: 2000,
+  rotate: { x: 0, y: 50, z: 0 },
+});
+
+ScrollReveal().reveal(".box-card-contatos", {
+  duration: 2000,
+  rotate: { x: 0, y: 50, z: 0 },
+});
+
+ScrollReveal().reveal(".box-formulario", {
+  duration: 2000,
+  rotate: { x: 0, y: 50, z: 0 },
+});
